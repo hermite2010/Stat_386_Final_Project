46931e5c36fbecc5badf796bd63a63a0d31a55b2
Joseph Bradley <hermite2010@gmail.com>
2023-11-30 15:16:41 -0700

    Streamlit update 5

--- Dashboard.py
+++ Dashboard.py
@@ -17,6 +17,7 @@ df.sort_values(by=x_axis, inplace=True, ascending=True)
 
 # Allow the user to select the schools to display
 schools = st.sidebar.multiselect('Select schools to display', df['School'].unique())
+df = df[df['School'].isin(schools)]
 
 # Plot the data using the selected X axis on a bar plot
 fig = px.bar(df, x=x_axis, y='School', color='Year')
